deb729508e13f84840c506dcfe6a21bae93bc089
Adam Hinkel <ahinkel421@gmail.com>
2017-08-25 12:20:10 -0400

    initial commit

--- models.js
+++ models.js
@@ -1,5 +1,27 @@
 const mongoose = require('mongoose');
 
 const blogPostSchema = mongoose.Schema({
-	
+	title: {type: String, required: true},
+	content: {type: String, required: true},
+	author: {
+		firstName: String, 
+		lastName: String
+	},
+	created: {type: Date}
 });
+
+blogPostSchema.virtual('authorString').get(function() {
+	return `${this.author.firstName} ${this.author.lastName}`.trim()});
+blogPostSchema.methods.apiRepr = function() {
+	return {
+		id: this._id,
+		title: this.title,
+		content: this.content,
+		author: this.authorString,
+		created: this.created
+	};
+}
+
+const blogPost = mongoose.model('blogPost', blogPostSchema);
+
+module.exports = {blogPost};
